441d82c13b73aa254702349890c13f21ed0cd8ba
Vaishnavi <vaishnavipadulkar@gmail.com>
2025-10-22 20:15:48 +0530

    server routes fix  commit

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 app.use(express.json());
 
 // Routes
@@ -22,9 +25,12 @@ app.use("/api/add", ContactRoute);
 app.use("/api/calculate", CalculateRoute);
 app.use("/api/admin", AdminRoute);
 
+
 // Test route
 app.get("/", (req, res) => res.send("API running"));
-
+app.use((req, res) => {
+    res.status(404).json({ message: "Resource Not Found." })
+})
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
